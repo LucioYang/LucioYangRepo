3bdf1e7f02e6642fe999c1677946c80d0472680e
Lucio Yang <lucioyang@Lucios-MBP.home>
2019-03-04 21:04:26 -0500

    commit message

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+/****************************************
+ *  File name:  HelloWorld.java
+ *  Compilation:  javac HelloWorld.kava
+ *  Execution:  java HelloWorld
+ *
+ *  Prints "hello, world". By tradition,
+ *  this is everyone's first program.
+****************************************/
+
+public class HelloWorld {
+  public static void main(String[] commandLine) {
+    System.out.println("hello, world");
+  }
+}
